68e87addda19170a9c1d898c6861e5b815205664
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2026-05-07 12:36:41 +0530

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -4,5 +4,5 @@ import { createContext } from "react";
 export const AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: "https://zune.onrender.com//"
+    baseURL: "https://zune.onrender.com/"
 })
